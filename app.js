429b6289360a4b441621661c242bd81f512b5c0d
Michelle Stermitz <stermitzm@gmail.com>
2020-07-09 16:33:30 -0700

    working JavaScript and tests

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 // import functions and grab DOM elements
-import { getRandomThrow } from './throwUtils.js';
+import { getRandomThrow, checkResult } from './throwUtils.js';
 
-const playButton = document.querySelector('button');
-const didYouWin = document.querySelector('win-lose');
+const playButton = document.querySelector('#button');
+const didYouWin = document.querySelector('#win-lose');
 
-// const totalWin = document.querySelector("total-wins");
-// const totalLoss = document.querySelector("total-losses");
-// const totalDraw = document.querySelectorAll(total-draws);
+const totalWin = document.querySelector('#total-wins');
+const totalLoss = document.querySelector('#total-losses');
+const totalDraw = document.querySelector('#total-draws');
 
 // initialize state
-// let wins = 0;
-// let losses = 0;
-// let draws = 0;
+let wins = 0;
+let draws = 0;
+let losses = 0;
 
 // set event listeners to update state and DOM
 playButton.addEventListener('click', () => {
-    const didYouWin = getRandomThrow();
-    console.log(didYouWin);
+    const computerThrow = getRandomThrow();
+    const userSelect = document.querySelector('input:checked');
+    const userThrow = userSelect.value;
+    const result = checkResult(userThrow, computerThrow);
+
+    if (result === 'win') {
+        wins++;
+        didYouWin.textContent = 'You won!';
+    } else if (result === 'lose') {
+        losses++;
+        didYouWin.textContent = 'You loser!';
+    } else if (result === 'draw') {
+        draws++;
+        didYouWin.textContent = "It's a draw!";
+    }
+
+    totalWin.textContent = 'Total Wins: ' + wins;
+    totalLoss.textContent = 'Total Losses: ' + losses;
+    totalDraw.textContent = 'Total Draws: ' + draws;
+
 });
 
 //compare user selection to the correct selection
