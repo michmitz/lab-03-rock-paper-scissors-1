5e676797c395c0852684f0329e8eb8b0fe99e980
Michelle Stermitz <stermitzm@gmail.com>
2020-07-09 15:07:49 -0700

    Added test

--- test/throwUtils.test.js
+++ test/throwUtils.test.js
@@ -1,4 +1,4 @@
-import { getRandomThrow } from '../throwUtils.js';
+import { getRandomThrow, checkResult } from '../throwUtils.js';
 
 const test = QUnit.test;
 
@@ -10,4 +10,13 @@ test('getRandomThrow function', (expect) => {
     //assert.equal(expected, actual === 'rock' || actual === 'paper' || actual === 'scissors');
     expect.ok(actual === 'rock' || actual === 'paper' || actual === 'scissors');
 });
+
+test('should return "draw" if passed "rock" and "rock"', (expect) => {
+    const expected = 'draw';
+
+    const actual = checkResult('rock', 'rock');
+
+    expect.equal(expected, actual);
+});
+
 
